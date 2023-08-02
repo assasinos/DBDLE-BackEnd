275b8f392c5c89e1ff5fdfe3c7bf68905e4a5a0e
assasinos <kubasem198@gmail.com>
2023-08-02 13:02:56 +0200

    Add DailyCharacter class

---
+++ Services/DailyCharacter/DailyCharacter.cs
@@ -0,0 +1,25 @@
+﻿using DBDLE_BackEnd.Models;
+
+namespace DBDLE_BackEnd.Services.DailyCharacter;
+
+public class DailyCharacter : IDailyCharacter
+{
+    
+    private CharacterModel? _dailyCharacter;
+    private readonly DailyCharacterConfigruation _configruation;
+
+    public DailyCharacter(DailyCharacterConfigruation configruation)
+    {
+        _configruation = configruation;
+    }
+
+
+    public CharacterModel? GetDailyCharacter() => _dailyCharacter;
+
+    public void UpdateDailyCharacter(CharacterModel? character)
+    {
+        _dailyCharacter = character;
+    }
+
+    public int GetMaxOffset() => _configruation.MaxOffset;
+}
