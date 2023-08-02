905c3507c845fa46188dfd03e5658510aeb73c9f
assasinos <kubasem198@gmail.com>
2023-08-02 13:10:49 +0200

    Create DailyCharacterUpdate Service

---
+++ Services/DailyCharacterUpdate/DailyCharacterUpdateService.cs
@@ -0,0 +1,26 @@
+﻿using DBDLE_BackEnd.Services.DailyCharacter;
+
+namespace DBDLE_BackEnd.Services.DailyCharacterUpdate;
+
+public class DailyCharacterUpdateService : BackgroundService
+{
+    private readonly IDailyCharacter _dailyCharacter;
+
+    public DailyCharacterUpdateService(IDailyCharacter dailyCharacter)
+    {
+        _dailyCharacter = dailyCharacter;
+    }
+
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var random = new Random();
+        while (!stoppingToken.IsCancellationRequested)
+        {
+
+            //Add random selection of character
+
+            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+        }
+    }
+}
