50a949954cf6d6edc0ef1dbabb3acda8cc117355
assasinos <kubasem198@gmail.com>
2023-08-03 16:04:46 +0200

    Implement DailyCharacter selection

--- Services/DailyCharacterUpdate/DailyCharacterUpdateService.cs
+++ Services/DailyCharacterUpdate/DailyCharacterUpdateService.cs
@@ -1,4 +1,7 @@
-﻿using DBDLE_BackEnd.Services.DailyCharacter;
+﻿using Dapper;
+using DBDLE_BackEnd.Models;
+using DBDLE_BackEnd.Services.DailyCharacter;
+using MySqlConnector;
 
 namespace DBDLE_BackEnd.Services.DailyCharacterUpdate;
 
@@ -6,9 +9,12 @@ public class DailyCharacterUpdateService : BackgroundService
 {
     private readonly IDailyCharacter _dailyCharacter;
 
-    public DailyCharacterUpdateService(IDailyCharacter dailyCharacter)
+    private readonly MySqlConnection _connection;
+    
+    public DailyCharacterUpdateService(IDailyCharacter dailyCharacter, MySqlConnection connection)
     {
         _dailyCharacter = dailyCharacter;
+        _connection = connection;
     }
 
 
@@ -20,6 +26,24 @@ public class DailyCharacterUpdateService : BackgroundService
 
             //Add random selection of character
 
+            var randomOffset = random.Next(0, _dailyCharacter.GetMaxOffset());
+
+            
+            //1->n relationship in case I want to serve image from backend or different server
+            var cmd = new CommandDefinition($@"SELECT C.CharacterName, C.Gender, C.Origin, C.Height, C.Difficulty, C.ReleaseYear, I.ImagePath FROM `Characters` C join Images I on I.ImageUID = C.ImageUID LIMIT 1 OFFSET {randomOffset}");
+
+            
+            
+            var character = (await _connection.QueryAsync<CharacterModel,ImageModel, CharacterModel>(cmd,
+                (model, imageModel) =>
+                {
+                    model.Image = imageModel;
+                    return model;
+                },splitOn:"ReleaseYear")).FirstOrDefault() ?? throw new Exception($"Could not find character with offset {randomOffset}");
+            _dailyCharacter.UpdateDailyCharacter(character);
+
+            
+            //1 minute for testing purposes, will be changed to 24 hours
             await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
         }
     }
