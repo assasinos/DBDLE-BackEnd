73df440ab01a613e4b7af0e864eddf6759f30455
assasinos <kubasem198@gmail.com>
2023-08-04 16:34:24 +0200

    Implement ConvertToJson function

--- Extensions/CharacterExtension.cs
+++ Extensions/CharacterExtension.cs
@@ -1,6 +1,24 @@
-﻿namespace DBDLE_BackEnd.Extensions;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using DBDLE_BackEnd.Models;
+
+namespace DBDLE_BackEnd.Extensions;
 
 public static class CharacterExtension
 {
-    
+
+
+    public static string ConvertToJson(this CharacterModel character)
+    {
+        return JsonSerializer.Serialize(character, new JsonSerializerOptions()
+        {
+            Converters =
+            {
+                new JsonStringEnumConverter()
+            },
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        });
+    }
+
+
 }
