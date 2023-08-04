6282aedc0001229cbabcabad4c78a711e29dcb7f
assasinos <kubasem198@gmail.com>
2023-08-04 16:48:16 +0200

    Implement GetAllCharacters action

--- Controllers/CharactersController.cs
+++ Controllers/CharactersController.cs
@@ -1,4 +1,6 @@
-﻿using DBDLE_BackEnd.Extensions;
+﻿using Dapper;
+using DBDLE_BackEnd.Extensions;
+using DBDLE_BackEnd.Models;
 using DBDLE_BackEnd.Services.DailyCharacter;
 using Microsoft.AspNetCore.Mvc;
 using MySqlConnector;
@@ -26,5 +28,23 @@ public class CharactersController : Controller
         //Convert to JSON bcs of the enum and null values,
         //Then convert to base64 to hide the data
         Ok( _dailyCharacter.GetDailyCharacter().ConvertToBase64Json());
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCharacters()
+    {
+        //1->n relationship in case I want to serve image from backend or different server
+        var cmd = new CommandDefinition($@"SELECT C.CharacterName, C.Gender, C.Origin, C.Height, C.Difficulty, C.ReleaseYear, I.ImagePath FROM `Characters` C join Images I on I.ImageUID = C.ImageUID");
+
+
+        var character = await _connection.QueryAsync<CharacterModel, ImageModel, CharacterModel>(cmd,
+            (model, imageModel) =>
+            {
+                model.Image = imageModel;
+                return model;
+            }, splitOn: "ImagePath");
+        //Convert to JSON bcs of the enum and null values
+        return Ok(character.ConvertToJson());
+    }
+    
     
 }
