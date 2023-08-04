f61f7f9239705c486c8486a4e022bef4231e3d68
assasinos <kubasem198@gmail.com>
2023-08-04 16:25:20 +0200

    Init Controller

---
+++ Controllers/CharactersController.cs
@@ -0,0 +1,24 @@
+﻿using DBDLE_BackEnd.Services.DailyCharacter;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+
+namespace DBDLE_BackEnd.Controllers;
+
+
+[Controller]
+[Route("api/[controller]/[action]")]
+public class CharactersController : Controller
+{
+    private readonly MySqlConnection _connection;
+    private readonly IDailyCharacter _dailyCharacter;
+    
+    
+    public CharactersController(MySqlConnection connection, IDailyCharacter dailyCharacter)
+    {
+        _connection = connection;
+        _dailyCharacter = dailyCharacter;
+    }
+    
+    
+
+}
